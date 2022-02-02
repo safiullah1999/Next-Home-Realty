9b17e228efbe6474cb0322c14e2e65ee8e1df336
safiullah1999 <safiullahsk1999@gmail.com>
2022-02-02 12:55:36 +0500

    Search filter functionality implemented

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,11 +3,44 @@ import { Flex, Box, Text, Icon, Spinner, Select, Input, Button } from "@chakra-u
 import { useRouter } from "next/router";
 import {MdCancel} from 'react-icons/md';
 
+import { filterData, getFilterValues } from "../utils/filterData";
+
 const SearchFilters = () => {
+  const [filters, setfilters] = useState(filterData);
+  const router = useRouter();
+
+  const searchProperties = (filtervalues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filtervalues);
+    
+    values.forEach((item)=>{
+      query[item.name] = item.value
+    })
+    // we update the url with pathname and query
+    router.push({ pathname: path, query })
+  }
+
   return(
-    <div>
-      searchFilters
-    </div>
+    <Flex bg={'gray.100'} justifyContent={'center'} flexWrap={'wrap'} >
+      {filters.map((filter)=>(
+        <Box key={filter.queryName}>
+          {/* onchange will trigger searchProperties function that will search specific properties that are related to selected filters in our query of url */}
+          <Select
+            placeholder={filter.placeholder}
+            p={2}
+            onChange={(e)=>searchProperties({ [filter.queryName]: e.target.value })}
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   )
 }
 
--- pages/search.js
+++ pages/search.js
@@ -3,9 +3,13 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import {BsFilter} from 'react-icons/bs';
-import SearchFilters from "../components/SearchFilters";
 
-const Search = () => {
+import SearchFilters from "../components/SearchFilters";
+import { fetchApi, baseUrl } from '../utils/fetchApi';
+import { Property } from '../components/Property';
+import noresult from '../assets/images/noresult.svg';
+
+const Search = ({ properties }) => {
   const [searchFilters, setsearchFilters] = useState(false);
   const router= useRouter();
 
@@ -26,8 +30,46 @@ const Search = () => {
         <Icon paddingLeft="2" w="7" as={BsFilter} />
       </Flex>
       {searchFilters && <SearchFilters />}
+      <Text fontSize={'2x1'} p={4} fontWeight={'bold'}>
+        Properties {router.query.purpose}
+      </Text>
+      <Flex flexWrap={'wrap'}>
+        {properties.map((property) => <Property property={property} key={property.id} /> )}
+      </Flex>
+      {properties.length === 0 &&(
+        <Flex justifyContent={"center"} alignItems={"center"} flexDirection={"column"} marginTop={5} marginBottom={5} >
+          <Image alt="No Result" src={noresult} />
+          <Text fontSize={'2x1'} marginTop={3} >No Result Found</Text>
+        </Flex>
+      )}
     </Box>
   )
 }
 
 export default Search;
+
+// in next js instead of using useEffect we use the below function.
+export async function getServerSideProps({query}) {
+  // we can do api calls
+  // const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose-for-sale&hitsPerPage=6`)
+  // next js automatically adds these below props as props for our component 
+  
+  const purpose = query.purpose || 'for-sale' //'for-sale' is the default value here.
+  const rentFrequency = query.rentFrequency || 'yearly'
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+  
+  const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
